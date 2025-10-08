9974486be6cc8ffebd0703545eb53654bda327f4
hama777 <GEB01604@nifty.ne.jp>
2025-10-08 20:25:07 +0900

    25/10/07 v0.06 月ごとのコミット数を表形式にした

--- commit.py
+++ commit.py
@@ -5,14 +5,44 @@ from datetime import date
 from datetime import datetime, timedelta
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
-# 25/08/06 v1.05 月ごとのコミット数集計
-version = "0.05"
+# 25/10/07 v0.06 月ごとのコミット数を表形式にした
+version = "0.06"
 
 appdir = os.path.dirname(os.path.abspath(__file__))
 conffile = appdir + "/repoview.conf"
 checkdate = appdir + "/checkdate.txt"
+templatefile = appdir + "/cmt_templ.htm"
+resultfile = appdir + "/commit.htm"
+
+commit_info = {}  # コミット情報   辞書  キー  repo名  値  コミット数
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def main_proc():
+    read_config()
+
+    if not proxy == "noproxy" :
+        os.environ['https_proxy'] = proxy
+    if not token:
+        print("Error: GitHub token is not set in the environment variables.")
+        return
+    
+
+    parse_template()
+    write_datetime()
+
+def monthly_commit_count() :
+    yy = 2025
+    today_date = date.today()   
+    today_mm = today_date.month
+    for mm in range(1,13) :
+        if mm > today_mm :
+            break
+        start_date =  date(2025, mm, 1)
+        last_day = calendar.monthrange(yy, mm)[1]   # 月の最終日
+        final_date = date(yy, mm, last_day)
+        n = get_period_commit_count(start_date,final_date)
+        out.write(f'<tr><td>{mm}</td><td>{n}</td><td></td></tr>\n')
 
 def get_repositories(username, token):
     url = f"https://api.github.com/users/{username}/repos"
@@ -28,6 +58,60 @@ def get_commit_counts(username, repo_name, token, since, until):
     response = requests.get(url, headers=headers, params=params, verify=False)
     response.raise_for_status()
     return len(response.json())
+
+def parse_template() :
+    global out 
+    f = open(templatefile , 'r', encoding='utf-8')
+    out = open(resultfile,'w' ,  encoding='utf-8')
+    for line in f :
+        if "%monthly_commit_count%" in line :
+            monthly_commit_count()
+            continue
+        if "%version%" in line :
+            s = line.replace("%version%",version)
+            out.write(s)
+            continue
+        out.write(line)
+
+    f.close()
+    out.close()
+
+def write_datetime() :
+    s = datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n"
+    f = open(checkdate,"w",encoding='utf-8')
+    f.write(s)
+    f.close()
+
+#   指定した期間のコミット数を取得する
+#   結果は辞書  commit_info  キー  repo名  値  コミット数
+def get_period_commit_count(start_date,end_date) :
+    count = 0 
+    repositories = get_repositories(username, token)
+
+    since = f"{start_date}T00:00:00Z"
+    until = f"{end_date}T23:59:59Z"
+    for repo in repositories:
+        count += get_commit_counts(username, repo, token, since, until)
+
+    return count
+
+#   指定した期間のコミット情報を取得する
+#   結果は辞書  commit_info  キー  repo名  値  コミット数
+def get_period_info(start_date,end_date) :
+    global commit_info
+
+    commit_info = {}
+    repositories = get_repositories(username, token)
+
+    since = f"{start_date}T00:00:00Z"
+    until = f"{end_date}T23:59:59Z"
+    for repo in repositories:
+        commit_count = get_commit_counts(username, repo, token, since, until)
+        if commit_count > 0:
+            commit_info[repo] = commit_count
+
+    print(commit_info)
+
 
 def read_config() : 
     global username,proxy,token,debug
@@ -48,55 +132,5 @@ def read_config() :
 
     conf.close()
 
-def write_datetime() :
-    s = datetime.now().strftime("%Y/%m/%d %H:%M:%S") + "\n"
-    f = open(checkdate,"w",encoding='utf-8')
-    f.write(s)
-    f.close()
-
-#   指定した期間のコミット情報を取得する
-#   結果は辞書  commit_info  キー  repo名  値  コミット数
-def get_period_info(start_date,end_date) :
-    global commit_info
-
-    commit_info = {}
-    repositories = get_repositories(username, token)
-
-    since = f"{start_date}T00:00:00Z"
-    until = f"{end_date}T23:59:59Z"
-    for repo in repositories:
-        commit_count = get_commit_counts(username, repo, token, since, until)
-        if commit_count > 0:
-            commit_info[repo] = commit_count
-
-    print(commit_info)
-
-def main():
-    read_config()
-
-    if not proxy == "noproxy" :
-        os.environ['https_proxy'] = proxy
-    if not token:
-        print("Error: GitHub token is not set in the environment variables.")
-        return
-    
-    today_date = date.today()   
-    today_mm = today_date.month
-
-    #start_date =  date(2025, 7, 1)
-    #end_date =  date(2025, 8, 5)
-    #get_period_info(start_date,end_date)
-
-    yy = 2025
-    for mm in range(1,13) :
-        if mm > today_mm :
-            break
-        start_date =  date(2025, mm, 1)
-        last_day = calendar.monthrange(yy, mm)[1]
-        final_date = date(yy, mm, last_day)
-        get_period_info(start_date,final_date)
-
-    write_datetime()
-
 if __name__ == "__main__":
-    main()
+    main_proc()
